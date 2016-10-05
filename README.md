88dee9ac5ccb1aa1dde46aeca160ae9061a61263
Dmitry <yankelevich.dima@gmail.com>
2016-10-05 13:29:37 +0300

    Added project template

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # python-space-invaders
+
+## О проекте
+
+Проект разбит на [клиентскую](client/README.md) и [серверную](server/README.md) части.
---
+++ client/client.py
@@ -0,0 +1,34 @@
+class APIConnector(object):
+    '''
+    Implements all communications between client and server
+    '''
+
+    def register_player():
+        pass
+
+    def login():
+        pass
+
+    def save_score():
+        pass
+
+
+class Game(object):
+    '''
+    Implements all logic and user interaction
+    '''
+    pass
+
+
+class Enemy(object):
+    '''
+    Enemy class
+    '''
+    pass
+
+
+class Player(object):
+    '''
+    Player class
+    '''
+    pass
